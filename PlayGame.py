137e4a7bc0c5e3bbc5eacdda54a8bc51540b69e3
Joseph Dobrow <jdobrow@Josephs-MacBook-Air.local>
2020-04-29 09:07:17 -0500

    restructured code

--- PlayGame.py
+++ PlayGame.py
@@ -1,13 +1,12 @@
-from TomBot import TomBot
-from ExampleBot import RandBot
-
-from Environment import Environment
 import random
-
+import Environment
 
 class PlayGame:
     
     def __init__(self, bot1, bot2, verbose=False):
+        
+        # Determine player1 and player2. Note: Player 1 loses a victory point tie.
+        
         flip = random.randint(1, 2)
         self.flip = flip
         if flip == 1:
@@ -17,15 +16,20 @@ class PlayGame:
             self.p2 = bot1
             self.p1 = bot2
             
-        self.env = Environment()
+        self.env = Environment.Environment()
         self.verbose = verbose
+        self.p1_deck, self.p2_deck = {}, {}
+        self.p1_hand, self.p2_hand = {}, {}
+        self.p1_discard, self.p2_discard = {}, {}
+        self.p1_move_list, self.p2_move_list = [], []
         
-        self.p1_deck = [7, 0, 0, 3, 0, 0, 0]
-        self.p2_deck = [7, 0, 0, 3, 0, 0, 0]
-        self.p1_hand= [0, 0, 0, 0, 0, 0, 0]
-        self.p2_hand= [0, 0, 0, 0, 0, 0, 0]
-        self.p1_discard = [0, 0, 0, 0, 0, 0, 0]
-        self.p2_discard = [0, 0, 0, 0, 0, 0, 0]
+        for card in self.env.card_map.keys():
+            self.p1_deck[card], self.p2_deck[card] = 0, 0
+            self.p1_hand[card], self.p2_hand[card] = 0, 0
+            self.p1_discard[card], self.p2_discard[card] = 0, 0
+            
+        self.p1_deck['copper'], self.p2_deck['copper'] = 7, 7
+        self.p1_deck['estate'], self.p2_deck['estate'] = 3, 3
         
         if verbose:
             print("Player 1: {}".format(self.p1.name))
@@ -33,61 +37,57 @@ class PlayGame:
         
     def play_game(self):
         
-        # Player 1 is referred to as 1, and Player 2 is -1
         self.clean_up(1)
         self.clean_up(-1)
         
-        for turn_number in range(1000):
+        for _ in range(5000):
             
             move = self.p1.get_moves(self.env, self.p1_deck, self.p1_hand, self.p1_discard)
-            if self.verbose:
-                print(self.p1.name + " buy a: " + get_card_name(move))
-            if move >= 0:
+            if move in self.env.card_map.keys():
                 self.p1_discard[move] += 1
-                self.env.card_counts[move] -= 1
+                self.env.card_map[move]['supply'] -= 1
+                self.p1_move_list.append(move)
             if self.env.check_win():
-                return [self.declare_winner(), turn_number]
+                return self.declare_winner()
             self.clean_up(1)
                 
             move = self.p2.get_moves(self.env, self.p2_deck, self.p2_hand, self.p2_discard)
-            if self.verbose:
-                print(self.p2.name + " buy a: " + get_card_name(move))
-            if move >= 0:
+            if move in self.env.card_map.keys():
                 self.p2_discard[move] += 1
-                self.env.card_counts[move] -= 1
+                self.env.card_map[move]['supply'] -= 1
+                self.p2_move_list.append(move)
             if self.env.check_win():
-                return [self.declare_winner(), turn_number]
+                return self.declare_winner()
             self.clean_up(-1)
 
+        return 'error'
                        
     def clean_up(self, player):
 
         if player == 1:
-            deck = self.p1_deck
-            discard = self.p1_discard
-            hand = self.p1_hand
+            deck, discard, hand = self.p1_deck, self.p1_discard, self.p1_hand
         else:
-            deck = self.p2_deck
-            discard = self.p2_discard
-            hand = self.p2_hand
+            deck, discard, hand = self.p2_deck, self.p2_discard, self.p2_hand
             
-        for i in range(len(hand)):
-            discard[i] += hand[i]
-            hand[i] = 0
+        for card in self.env.card_map.keys():
+            card_amount = hand[card]
+            discard[card] += card_amount
+            hand[card] -= card_amount
             
         for i in range(5):
-            if sum(deck) < 1:
-                for i in range(len(discard)):
-                    deck[i] += discard[i]
-                    discard[i] = 0
-            if sum(deck) > 0:
-                draw = random.randint(1, sum(deck))
-                for j in range(7):
-                    if draw <= deck[j]:
-                        deck[j] -= 1
-                        hand[j] += 1
+            if sum(deck.values()) < 1:
+                for card in self.env.card_map.keys():
+                    card_amount = discard[card]
+                    discard[card] -= card_amount
+                    deck[card] += card_amount
+            if sum(deck.values()) > 0:
+                draw = random.randint(1, sum(deck.values()))
+                for card in self.env.card_map.keys():
+                    if draw <= deck[card]:
+                        deck[card] -= 1
+                        hand[card] += 1
                         break
-                    draw -= deck[j]
+                    draw -= deck[card]
     
     
     def declare_winner(self):
@@ -103,28 +103,21 @@ class PlayGame:
                 return -1
             else:
                 return 1
-    
+        
     def get_vp(self, player):
         
-        if player == 1:
-            return self.p1_deck[3]+self.p1_deck[4]*3+self.p1_deck[5]*6+self.p1_hand[3]+self.p1_hand[4]*3+self.p1_hand[5]*6+self.p1_discard[3]+self.p1_discard[4]*3+self.p1_discard[5]*6
-        else:
-            return self.p2_deck[3]+self.p2_deck[4]*3+self.p2_deck[5]*6+self.p2_hand[3]+self.p2_hand[4]*3+self.p2_hand[5]*6+self.p2_discard[3]+self.p2_discard[4]*3+self.p2_discard[5]*6
+        vp_total = 0
         
-def get_card_name(index):
-    if index == -1:
-        return 'No Buy'
-    return ['Copper', 'Silver', 'Gold', 'Estate', 'Duchy', 'Province', 'Curse'][index]
-
-
-tom_wins = 0
-joe_wins = 0
-for _ in range(100):
-    result = PlayGame(TomBot(), RandBot(), True).play_game()
-    if result[0] == 1:
-        tom_wins += 1
-    else:
-        joe_wins += 1
-print("Tom: " + str(tom_wins))
-print("Joe: " + str(joe_wins))
-
+        if player == 1:
+            for card in self.env.card_map.keys():
+                vp_total += self.p1_deck[card] * self.env.card_map[card]['vp']
+                vp_total += self.p1_discard[card] * self.env.card_map[card]['vp']
+                vp_total += self.p1_hand[card] * self.env.card_map[card]['vp']
+            return vp_total
+        
+        else:
+            for card in self.env.card_map.keys():
+                vp_total += self.p2_deck[card] * self.env.card_map[card]['vp']
+                vp_total += self.p2_discard[card] * self.env.card_map[card]['vp']
+                vp_total += self.p2_hand[card] * self.env.card_map[card]['vp']
+            return vp_total
